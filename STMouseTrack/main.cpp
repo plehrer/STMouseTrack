615b407653bd358a16e17dfd0ef3abbbd89586ab
plehrer <plehrer@gmail.com>
2014-04-09 10:37:28 -0400

    Initial Commit

---
+++ STMouseTrack/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  STMouseTrack
+//
+//  Created by Peter Lehrer on 4/9/14.
+//  Copyright (c) 2014 Brergen Community College STEM. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
